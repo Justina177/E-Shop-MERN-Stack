aa3042364b76d3f594501615851f98781cd6d31a
justina177 <justinaenenche@yahoo.com>
2023-09-11 01:25:43 +0100

    Implemented routing to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Layout from './Components/Layout/Layout';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
     <>
+    
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
+        
       </Routes>
     </BrowserRouter>
     </>
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
   )
 }
 
